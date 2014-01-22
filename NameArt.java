fed4d0129cc5e48dfc49be74838ca865457c1cd5
salazaraj2 <salazaraj2@vcu.edu>
2014-01-21 20:48:17 -0500

    printHeadingtemplate

---
+++ NameArt.java
@@ -0,0 +1,20 @@
+//***************************************************************************
+// Alec Salazar
+// CMSC255-901
+// 2014JAN21
+// Project 1 - Your Name in Code
+//***************************************************************************
+
+public class NameArt{
+
+	private static void printHeading(){
+		System.out.println("Alec Salazar");
+		System.out.println("CMSC255-901 Spring 2014");
+		System.out.println("Programming Project 1");
+		System.out.println("Your Name in Code");
+		System.out.println();
+	}
+
+	public static void main (String[] args){
+		printHeading(); // this statement calls the printHeading method
+		System.out.println(
