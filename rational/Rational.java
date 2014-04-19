f76d528ea6d8baefd93d556fd1bd286658e211a4
salazaraj2 <salazaraj2@vcu.edu>
2014-04-19 17:27:30 -0400

    Project Rational

--- rational/Rational.java
+++ rational/Rational.java
@@ -1,71 +1,111 @@
 /*
  * Alec Salazar
  * CMSC255-901
- * 2014APR17
+ * 2014APR19
  * Project 7 - Rational
  */
-
+/**This class represents a rational number, an integer over another integer*/
 public class Rational{
-	private int num, dnm;
+	private int num, dnm; //Recommend using long for actual production
+	private boolean hasZero = false;
+	/**Default constructor sets the rational to 1/1*/
 	public Rational(){
-		num = 1;
-		dnm = 1;
+		setNum(1);
+		setDnm(1);
 	}
-	public Rational(int n1, int n2){
-		num = n1;
-		dnm = n2;
-		Normalize();
+	/**Paramaterized constructor takes two ints, one for numerator and one for denominator
+	 * then calls normalize*/
+	public Rational(int n, int d){
+		setNum(n);
+		setDnm(d);
+		normalize();
 	}
-	private void Normalize(){
-		if(dnm < 0){
-			if(num < 0){
-				num = Math.abs(num);
-				dnm = Math.abs(dnm);
+	/**Method to remove duplicate negatives, or move the negative to the numerator*/
+	private void normalize(){
+		if(getDnm() < 0){
+			if(getNum() < 0){
+				setNum(Math.abs(getNum()));
+				setDnm(Math.abs(getDnm()));
 			}
 			else{
-				num = 0 - num;
-				dnm = Math.abs(dnm);
+				setNum(0 - getNum());
+				setDnm(Math.abs(getDnm()));
 			}
 		}
 	}
 
+	/**Method to get the numerator in the rational number*/
 	public int getNum(){
 		return num;
 	}
+	/**Method to get the denominator in the rational number*/
 	public int getDnm(){
 		return dnm;
 	}
-	public static boolean isRational(Object obj){
+	/**Method to set the numerator in the rational number*/
+	public void setNum(int n){
+		num = n;
+	}
+	/**Method to set the denominator in the rational number*/
+	public void setDnm(int d){
+		dnm = d;
+	}
+	/**Method to check if an Object is of the Rational class*/
+	public boolean isRational(Object obj){
 		if(this == obj) return true;
 		if(obj == null) return false;
 		if(!(obj instanceof Rational)) return false;
 		return true;
 	}
+	/**Method to compare two rational numbers for equality*/
 	public boolean equals(Object other){
 		if(!isRational(other)) return false;
 		Rational rat2 = (Rational) other;
-		if(num * rat2.getDnm() == dnm * rat2.getNum()) return true;
+		if(getNum() * rat2.getDnm() == getDnm() * rat2.getNum()) return true;
 		return false;
 	}
+	/**Method to display the two components of a rational number as a string*/
 	public String toString(){
-		System.out.print("(" + num + "/" + dnm + ")");
+		return "(" + getNum() + "/" + getDnm() + ")";
 	}
 
+	/**Method to add two rational numbers*/
 	public void add(Rational rat2){
-		num = num * rat2.getDnm() + dnm * rat2.getNum(); 
-		dnm = dnm * rat2.getDnm();
+		setNum(getNum() * rat2.getDnm() + getDnm() * rat2.getNum()); 
+		setDnm(getDnm() * rat2.getDnm());
 	}
+	/**Method to subtract two rational numbers*/
 	public void subtract(Rational rat2){
-		num = num * rat2.getDnm() - dnm * rat2.getNum();
-		dnm = dnm * rat2.getDnm();
+		setNum(getNum() * rat2.getDnm() - getDnm() * rat2.getNum());
+		setDnm(getDnm() * rat2.getDnm());
 	}
+	/**Method to multiply two rational numbers*/
 	public void multiply(Rational rat2){
-		num = num * rat2.getNum();
-		dnm = dnm * rat2.getDnm();
+		setNum(getNum() * rat2.getNum());
+		setDnm(getDnm() * rat2.getDnm());
 	}
+	/**Method to divide two rational numbers*/
 	public void divide(Rational rat2){
-		num = num * rat2.getDnm();
-		dnm = dnm * rat2.getNum();
+		setNum(getNum() * rat2.getDnm());
+		setDnm(getDnm() * rat2.getNum());
+	}
+	/**This method implements the Euclidean algorithm to find the gcd*/
+	public void factor(){
+		normalize();//On the chance that the neg's will disappear
+		int a = Math.abs(getNum());//We're looking for common factors, so don't care about sign here
+		int b = Math.abs(getDnm());
+		if(b > a){ //Set a to be the larger
+			int larger = b;
+			b = a;
+			a = larger;
+		}
+		while(b != 0){
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		setNum(getNum() / a);
+		setDnm(getDnm() / a);
 	}
 
 	public static void main(String args[]){
--- rational/RationalDriver.java
+++ rational/RationalDriver.java
@@ -1,15 +1,106 @@
-public class RationaDriver{
-	public static void menu{
-		System.out.print();
+/*
+ * Alec Salazar
+ * CMSC255-901
+ * 2014APR19
+ * Project 7 - Rational
+ */
+import java.lang.Character;
+import java.util.Scanner;
+
+/**This is the tester for the Rational class*/
+public class RationalDriver{
+	/**A method to display the menu to the user and receive the user's input
+	 * @return the user's selection*/
+	public static char menu(){
+		Scanner in = new Scanner(System.in);
+		char choice = '5';
+		System.out.println("Enter the corresponding number for the desired action");
+		System.out.println("1. Addition");
+		System.out.println("2. Subtraction");
+		System.out.println("3. Multiplication");
+		System.out.println("4. Division");
+		System.out.println("5. Test for Equality");
+		System.out.println("6. Change 1st rational number");
+		System.out.println("7. Change 2nd rational number");
+		System.out.println("(Q/q/X/x/Y/y) to exit");
+		System.out.println("Any other key for menu");
+		choice = in.next().charAt(0);
+		return choice;
 	}
+	/**Establish two rational objects and allow the user to perform basic arithmetic
+	 * operations and directly modify or compare the rational numbers*/
 	public static void main (String args[]){
-		printHeading();
-		Rational rat1 = new Rational(), rat2 = new Rational ();
+		Rational.printHeading();
+		int num = 1, dnm = 1;
+		char choice = 'm';
+		Scanner in = new Scanner(System.in);
+		Rational r1 = new Rational(), r2 = new Rational();
+		r1.setNum(nonZero('1', "numerator"));
+		r1.setDnm(nonZero('1', "denominator"));
+		r1.factor();
+		System.out.println("Rational Number #1: " + r1.toString());
+
+		r2.setNum(nonZero('2', "numerator"));
+		r2.setDnm(nonZero('2', "denominator"));
+		r2.factor();
+		System.out.println("Rational Number #2: " + r2.toString());
 		do{
-			System.out.print("Enter the numerator for rational number #1: ");
-		}while(!hasNextInt());
-		num = nextInt();
+			if(choice < '1' || choice > '7') choice = menu();
+			if(choice >= '1' && choice <= '4'){
+				System.out.print(r1.toString());
+				if(choice == '1'){
+					System.out.println(" + " + r2.toString() + " = ");
+					r1.add(r2);
+				}	
+				else if(choice == '2'){
+					System.out.println(" - " + r2.toString() + " = ");
+					r1.subtract(r2);
+				}	
+				else if(choice == '3'){
+					System.out.println(" * " + r2.toString() + " = ");
+					r1.multiply(r2);
+				}	
+				else if(choice == '4'){
+					System.out.println(" / " + r2.toString() + " = ");
+					r1.divide(r2);
+				}	
+				r1.factor();
+				System.out.print(r1.toString() + "\n");
+			}
+			else if(choice == '5'){
+				System.out.println(r1.toString() + " = " + r2.toString() + " : " + r1.equals(r2));
+			}	
+			else if(choice == '6'){
+				r1.setNum(nonZero('1', "numerator"));
+				r1.setDnm(nonZero('1', "denominator"));
+				r1.factor();
+				System.out.println("Rational Number #1: " + r1.toString());
+			}	
+			else if(choice == '7'){
+				r2.setNum(nonZero('2', "numerator"));
+				r2.setDnm(nonZero('2', "denominator"));
+				r2.factor();
+				System.out.println("Rational Number #2: " + r2.toString());
+			}	
+			System.out.print("Select (m)enu entry or exit: ");
+			choice = in.next().charAt(0);
+		}while(choice != 'y' && choice != 'Y' && choice != 'q' && choice != 'Q' && choice != 'x' && choice != 'X');
+	}
+
+	/**This method prompts the user to input a nonzero number
+	 * @param char which rational number to modify
+	 * @param String which part of the rational number to modify
+	 * @return the value recieved from the user
+	 */
+	public static int nonZero(char first, String selection){
+		Scanner in = new Scanner(System.in);
+		int number = 0;
 		do{
-			System.out.print("Enter the denominator for rational number #1: ");
-		}while(!hasNextInt());
-		dnm = nextInt();
+			System.out.print("Enter the " + selection + " for rational number #" + first + ": ");
+			while(!in.hasNextInt())	in.next();
+			number = in.nextInt();
+			if(number == 0)	System.out.println("Number must be nonzero.");
+		}while(number == 0);
+		return number;
+	}
+}
